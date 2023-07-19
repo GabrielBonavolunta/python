36da5e4c166b6eb6dfa5cfc48c2d9ffef8739793
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2023-07-19 16:17:55 -0300

    Commit aulas 13-18

---
+++ aula13.py
@@ -0,0 +1,12 @@
+nome = "Gabriel Bonavoluntá"
+altura = 1.67
+peso = 80
+imc = peso / (altura * altura)
+
+linha_1 = f"{nome} tem {altura:.2f} de altura"
+linha_2 = f"pesa {peso} quilos e seu imc é de"
+linha_3 = f"{imc:.2f}"
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
---
+++ aula17.py
@@ -0,0 +1,25 @@
+# if/ elif    / else
+# se/ senão se/ senão
+
+condicao_1 = True
+condicao_2 = True
+condicao_3 = True
+condicao_4 = True
+
+if condicao_1:
+    print("Código para condição 1")
+elif condicao_2:
+    print("Código para condição 2")
+elif condicao_3:
+    print("Código para condição 3")
+elif condicao_4:
+    print("Código para condição 4")
+else:
+    print("Nenhuma condição foi satisfeita.")
+
+
+if 10 == 10:
+    print("Outro if")
+
+
+print("Fora do if")
