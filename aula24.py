cf575435380c6d4e2e6692d282899308d9ff7310
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2023-07-24 17:35:38 -0300

    Commit aulas 23-29

--- aula24.py
+++ aula24.py
@@ -1,12 +1,21 @@
-# Operador lógico "not"
-# Usado para inverter expressões
-# not True = False
-# not False = True
+# Operadores in e not in
+# Strings são iteráveis
+#  0 1 2 3 4 5
+#  O t á v i o
+# -6-5-4-3-2-1
 
-# senha = input("Senha: ")
+# nome = "Otávio"
+# print(nome[2])
+# print(nome[-4])
+# print("vio" in nome)
+# print("zero" in nome)
+# print("vio" not in nome)
+# print("zero" not in nome)
 
-# if not senha:
-#    print("Senha incorreta")
+nome = input("Digite seu nome: ")
+encontrar = input("Digite o que deseja encontrar: ")
 
-print(not True)
-print(not False)
+if encontrar in nome:
+    print(f"{encontrar} está em {nome}.")
+else:
+    print(f"{encontrar} não está em {nome}.")
--- aula25.py
+++ aula25.py
@@ -1,21 +1,13 @@
-# Operadores in e not in
-# Strings são iteráveis
-#  0 1 2 3 4 5
-#  O t á v i o
-# -6-5-4-3-2-1
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
 
-# nome = "Otávio"
-# print(nome[2])
-# print(nome[-4])
-# print("vio" in nome)
-# print("zero" in nome)
-# print("vio" not in nome)
-# print("zero" not in nome)
-
-nome = input("Digite seu nome: ")
-encontrar = input("Digite o que deseja encontrar: ")
-
-if encontrar in nome:
-    print(f"{encontrar} está em {nome}.")
-else:
-    print(f"{encontrar} não está em {nome}.")
+nome = "Luiz"
+preco = 1000.95897643
+variavel = "%s, o preço total foi R$%.2f" % (nome, preco)
+print(variavel)
+print("O hexadecimal de %d é %04x" % (15, 15))
