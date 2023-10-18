641aa212f257adfd76d8b77817d45bbe1b5e489c
Gabriel Bonavoluntá <gabriel.bonavolonta@hotmail.com>
2023-10-18 19:05:08 -0300

    Commit aulas 97-99

--- aula97.py
+++ aula97.py
@@ -8,5 +8,11 @@
 # O python conhece todos os módulos e pacotes presentes
 # nos caminhos de sys.path
 import aula97_m
+from aula97_m import soma, variavel_modulo
 
-print("Esse módulo se chama", __name__)
+print('Este módulo se chama', __name__)
+# print('Este módulo se chama', __name__)
+print(aula97_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2, 3))
+print(aula97_m.soma(2, 3))
--- aula97_m.py
+++ aula97_m.py
@@ -1,1 +1,4 @@
-print("Esse módulo se chama", __name__)
+variavel_modulo = "Luiz"
+
+def soma(x, y):
+    return x + y
